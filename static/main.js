d12575c0af283f08cd6db847f2663796b88725ec
charalambosp <papaloizouc@gmail.com>
2014-06-21 22:40:41 +0100

    start drawing from the center in order to make a graph

--- static/main.js
+++ static/main.js
@@ -1,9 +1,7 @@
 initGraph = function () {
     var draw = SVG('drawing').size(800, 800);
     var rect = draw.rect(100, 100).attr({ fill: '#f06' });
-
-    draw.line(0, 0, 100, 150).stroke({ width: 1 });
-    var ellipse = draw.ellipse(80, 40).move(10, 10).fill({ color: '#fff' });
-
-    rect.maskWith(ellipse);
+    var cx = rect.cx();
+    var cy = rect.cy();
+    draw.line(cx, cy, 100, 150).stroke({ width: 1 });
 };
