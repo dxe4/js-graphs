5debf63a69b94f68153fed105f11d771bc91a9ef
charalambosp <papaloizouc@gmail.com>
2014-06-21 23:27:05 +0100

    connect centers

--- static/main.js
+++ static/main.js
@@ -10,8 +10,30 @@ var safeDevide = function (a, b, returnValue) {
 };
 
 var slope = function (x, y, x1, y1) {
-    return safeDevide(y-y1, x-x1, "vertical");
+    return safeDevide(y - y1, x - x1, "vertical");
 };
+
+var makeLine = function (cx, cy, cx1, cy1) {
+    /**
+     *  Reinventing the wheel. For more info read here:
+     *  http://en.wikipedia.org/wiki/Reinventing_the_wheel
+     */
+    var line = [];
+    var temp_x = cx,
+        temp_y = cy,
+        dx = cx - cx1,
+        dy = cy - cy1;
+
+    var dx_ratio = Math.floor(dx / Math.pow(cx1 - cx, cx1 - cx));
+    var dy_ratio = Math.floor(dy / Math.pow(cy1 - cy, cy1 - cy));
+
+    while (temp_x >= cx1 && temp_y >= cy1) {
+        temp_x = temp_x + dx_ratio;
+        temp_y = temp_y + dy_ratio;
+        line.push([temp_x, temp_y]);
+    }
+};
+
 
 initGraph = function () {
     var draw = SVG('drawing').size(800, 800);
@@ -25,13 +47,5 @@ initGraph = function () {
     var cx1 = rect2.cx();
     var cy1 = rect2.cy();
 
-    for (i = cx; i <= cx1; i++) {
-        for (j = cy; j <= cy1; j++) {
-
-        }
-    }
-
-    alert(slope(cx,cy,cx1,cy1));
-
-    draw.line(cx, cy, 100, 150).stroke({ width: 1 });
+    draw.line(cx, cy, cx1, cy1).stroke({ width: 1 });
 };
