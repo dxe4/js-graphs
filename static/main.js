b95d0b7ef2cbb6c2b84e346a4349006377fd7965
charalambosp <papaloizouc@gmail.com>
2014-06-21 22:58:58 +0100

    started a line generator

--- static/main.js
+++ static/main.js
@@ -15,17 +15,23 @@ var slope = function (x, y, x1, y1) {
 
 initGraph = function () {
     var draw = SVG('drawing').size(800, 800);
+
     var rect = draw.rect(100, 100).attr({ fill: '#f06' });
+    var rect2 = draw.rect(100, 100).attr({ fill: '#f15' }).move(100, 0);
 
     var cx = rect.cx();
     var cy = rect.cy();
 
+    var cx1 = rect2.cx();
+    var cy1 = rect2.cy();
 
-    var rect2 = draw.rect(100, 100).attr({ fill: '#f15' }).move(150, 150);
+    for (i = cx; i <= cx1; i++) {
+        for (j = cy; j <= cy1; j++) {
 
-    var cx1 = rect.cx();
-    var cy1 = rect.cy();
+        }
+    }
 
+    alert(slope(cx,cy,cx1,cy1));
 
     draw.line(cx, cy, 100, 150).stroke({ width: 1 });
 };
