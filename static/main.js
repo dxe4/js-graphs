a33110c0a56d6371da550309e83979297153f652
charalambosp <papaloizouc@gmail.com>
2014-06-22 01:38:07 +0100

    move unused code

--- static/main.js
+++ static/main.js
@@ -1,39 +1,3 @@
-var safeDevide = function (a, b, returnValue) {
-    if (returnValue === undefined) {
-        returnValue = 0;
-    }
-    if (b === 0) {
-        return returnValue;
-    } else {
-        return a / b;
-    }
-};
-
-var slope = function (x, y, x1, y1) {
-    return safeDevide(y - y1, x - x1, "vertical");
-};
-
-var makeLine = function (cx, cy, cx1, cy1) {
-    /**
-     *  Reinventing the wheel. For more info read here:
-     *  http://en.wikipedia.org/wiki/Reinventing_the_wheel
-     */
-    var line = [];
-    var temp_x = cx,
-        temp_y = cy,
-        dx = cx - cx1,
-        dy = cy - cy1;
-
-    var dx_ratio = Math.floor(dx / Math.pow(cx1 - cx, cx1 - cx));
-    var dy_ratio = Math.floor(dy / Math.pow(cy1 - cy, cy1 - cy));
-
-    while (temp_x >= cx1 && temp_y >= cy1) {
-        temp_x = temp_x + dx_ratio;
-        temp_y = temp_y + dy_ratio;
-        line.push([temp_x, temp_y]);
-    }
-};
-
 connectLines = function (draw, shape_a, shape_b) {
     var cx = shape_a.cx();
     var cy = shape_a.cy();
@@ -91,6 +55,47 @@ var fMakeTestData = function (max_x, max_y, rec_size) {
      */
     return product(_.range(0, max_x, rec_size), _.range(0, max_y, rec_size));
 };
+
+////////////////////////////////////////////////////
+//    UNUSED CODE THAT MAY BE USED LATE ON       ///
+////////////////////////////////////////////////////
+
+var safeDevide = function (a, b, returnValue) {
+    if (returnValue === undefined) {
+        returnValue = 0;
+    }
+    if (b === 0) {
+        return returnValue;
+    } else {
+        return a / b;
+    }
+};
+
+var slope = function (x, y, x1, y1) {
+    return safeDevide(y - y1, x - x1, "vertical");
+};
+
+var makeLine = function (cx, cy, cx1, cy1) {
+    /**
+     *  Reinventing the wheel. For more info read here:
+     *  http://en.wikipedia.org/wiki/Reinventing_the_wheel
+     */
+    var line = [];
+    var temp_x = cx,
+        temp_y = cy,
+        dx = cx - cx1,
+        dy = cy - cy1;
+
+    var dx_ratio = Math.floor(dx / Math.pow(cx1 - cx, cx1 - cx));
+    var dy_ratio = Math.floor(dy / Math.pow(cy1 - cy, cy1 - cy));
+
+    while (temp_x >= cx1 && temp_y >= cy1) {
+        temp_x = temp_x + dx_ratio;
+        temp_y = temp_y + dy_ratio;
+        line.push([temp_x, temp_y]);
+    }
+};
+
 
 var increaseValue = function (current, increase, max) {
     return (current > max) ? 0 : current + increase;
