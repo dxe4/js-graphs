ed41405a2043f962dd8760f35e3fbb5551cb4bf4
charalambosp <papaloizouc@gmail.com>
2014-06-28 14:42:10 +0100

    fixing project structure

--- main.py
+++
@@ -1,8 +0,0 @@
-from bottle import route, run, template, static_file
-
-
-@route('/<filename>')
-def server_static(filename):
-    return static_file(filename, root='./static')
-
-run(host='localhost', port=8080)
